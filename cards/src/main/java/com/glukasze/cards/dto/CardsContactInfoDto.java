b7075db746847d5dae1d9b3348afe2f9f863d87a
Glukasze <Mortal^66>
2025-07-13 13:19:57 +0200

    Configuration - Add actuator config

--- cards/src/main/java/com/glukasze/cards/dto/CardsContactInfoDto.java
+++ cards/src/main/java/com/glukasze/cards/dto/CardsContactInfoDto.java
@@ -1,9 +1,16 @@
 package com.glukasze.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.Map;
 
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails) {
+public class CardsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
 }
